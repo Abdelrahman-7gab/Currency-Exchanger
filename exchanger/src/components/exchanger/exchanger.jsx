53cf6c9ce0a0b50dc6980e95461555b559533881
Abdelrahman Hegab <abdulrahman.hegab@tensorgraph.io>
2022-07-14 04:05:50 +0200

    working view and added database with redis caching

--- exchanger/src/components/exchanger/exchanger.jsx
+++ exchanger/src/components/exchanger/exchanger.jsx
@@ -5,56 +5,107 @@ import TextField from "@mui/material/TextField";
 import { currencies } from "./currencies";
 import { useState } from "react";
 import { useEffect } from "react";
-import * as APIservice from "../../services/APIservice";
+import apiService from "../../services/APIservice";
+import SwitchAccessShortcutIcon from "@mui/icons-material/SwitchAccessShortcut";
 
 function exchanger() {
   const [fromCurrency, setFromCurrency] = useState("");
   const [toCurrency, setToCurrency] = useState("");
-  const [fromAmount, setFromAmount] = useState("");
-  const [toAmount, setToAmount] = useState("");
+  const [fromAmount, setFromAmount] = useState(1);
   const [conversionRate, setConversionRate] = useState(0);
 
-  const handleFromAmountChange = (event) => {
-    setFromAmount(event.target.value);
-  }
+  useEffect(() => {
+    const getConversionRate = async () => {
+      if (fromCurrency !== "" && toCurrency != "") {
+        const conversionRate = await apiService.getConversionRate(
+          fromCurrency.substring(0, 3),
+          toCurrency.substring(0, 3)
+        );
+        setConversionRate(conversionRate.data.info.rate);
+      }
+    };
+    getConversionRate();
+  }, [fromCurrency, toCurrency]);
 
-  const handleToAmountChange = (event) => {
-    setToAmount(event.target.value);
-  }
+  const changeAmount = (event) => {
+    setFromAmount(event.target.value);
+  };
+
+  const switchFromAndTo = () => {
+    const temp = fromCurrency;
+    const tempTo = toCurrency;
+    setFromCurrency("");
+    setToCurrency(temp);
+    setFromCurrency(tempTo);
+  };
 
   return (
     <div className="exchange-container">
-      <Autocomplete
-        id="From"
-        disableClearable
-        options={currencies}
-        renderInput={(params) => (
+      <div className="inputs-container">
+        <div className="from-container">
           <TextField
             className="exchange-input"
-            {...params}
-            label="from"
-
+            label="amount"
             color="warning"
+            value={fromAmount}
+            type="number"
+            InputProps={{
+              inputProps: { min: 0 },
+            }}
+            onChange={(e) => setFromAmount(e.target.value)}
           />
-        )}
-      />
 
-      <Autocomplete
-        id="To"
-        disableClearable
-        options={currencies}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="To"
-
-            color="warning"
+          <Autocomplete
+            id="From"
+            disableClearable
+            options={currencies}
+            onChange={(event, value) => setFromCurrency(value)}
+            value={fromCurrency}
+            renderInput={(params) => (
+              <TextField
+                className="exchange-input"
+                {...params}
+                label="from"
+                color="warning"
+              />
+            )}
           />
+        </div>
+        <div
+          className="switch"
+          onClick={switchFromAndTo}
+          style={{ margin: "0 auto", cursor: "pointer" }}
+        >
+          <SwitchAccessShortcutIcon style={{ color: "black" }} />
+        </div>
+        <Autocomplete
+          id="To"
+          disableClearable
+          options={currencies}
+          onChange={(event, value) => setToCurrency(value)}
+          value={toCurrency}
+          renderInput={(params) => (
+            <TextField {...params} label="To" color="warning" />
+          )}
+          inputprops={{
+            className: "exchange-input",
+          }}
+        />
+      </div>
+
+      {fromAmount != "" &&
+        fromAmount > 0 &&
+        fromCurrency != "" &&
+        toCurrency != "" && (
+          <div className="status">
+            {fromAmount} {fromCurrency.substring(5, fromCurrency.length - 1)} ={" "}
+            <span className="conversionRate">
+              {" "}
+              {conversionRate * fromAmount}{" "}
+            </span>{" "}
+            {toCurrency.substring(5, toCurrency.length - 1)}
+          </div>
         )}
-        inputprops={{
-          className: "exchange-input",
-        }}
-      />
     </div>
   );
 }
--- exchanger/src/services/APIservice.js
+++ exchanger/src/services/APIservice.js
@@ -1,16 +1,13 @@
 import axios from "axios";
 
-const convert = async (fromCurrency, toCurrency, amount) => {
+const apiService = {
+  getConversionRate: async (fromCurrency, toCurrency) =>{
   const converstionRate = await axios.get(
-    `https://api.apilayer.com/fixer/convert?to=${toCurrency}&from=${fromCurrency}&amount=${amount}`,
-    {
-      headers: {
-        apiKey: import.meta.env.FLIXER_API_TOKEN,
-      },
-    }
+    `http://localhost:3000/conversionRate/${fromCurrency}/${toCurrency}`,
   );
 
   return converstionRate;
-};
+}
+}
 
-export default { convert };
+export default apiService;
